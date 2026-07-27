78c82024b31e19fea8703da4707144e7a898775a
Sumit Avasthi <sumitavasthi.28@gmail.com>
2026-07-27 21:20:39 +0530

    Creating setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from setuptools import setup,find_packages
+from typing import List
+
+
+def give_requirements(file_name:str)->List[str]:
+    requirements:List[str] = []
+    try:
+        with open(file_name,'r') as file_obj:
+
+            lines = file_obj.readlines()
+
+            for line in lines:
+
+                requirement = line.strip()
+
+                if requirement and requirement != '-e .':
+
+                    requirements.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt is not found !")
+
+    return requirements
 
 
 setup(
@@ -7,5 +28,5 @@ setup(
     author="Sumit Avasthi",
     author_email="sumitavasthi.28@gmail.com",
     packages=find_packages(),
-    
+    install_requires=give_requirements('requirements.txt')
 )
