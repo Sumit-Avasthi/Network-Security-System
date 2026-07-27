05fe37f15f0404f3bbdac4aecf1d09a5d9a269ac
Sumit Avasthi <sumitavasthi.28@gmail.com>
2026-07-27 20:42:10 +0530

    Initial Structure Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name="Network Security",
+    version="0.0.1",
+    author="Sumit Avasthi",
+    author_email="sumitavasthi.28@gmail.com",
+    packages=find_packages(),
+    
+)
