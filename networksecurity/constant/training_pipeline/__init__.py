a02a89d385284a035bdfc39a3b0381047e370837
Sumit Avasthi <sumitavasthi.28@gmail.com>
2026-07-30 10:41:58 +0530

    Data Ingestion Configuration

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import numpy as np
+import sys
+
+
+TARGET_COLUMN="Result"
+PIPELINE_NAME:str="NetworkSecurity"
+ARTIFACTS_DIR:str="Artifacts"
+
+### Inside Feature store folder (raw data)
+FILE_NAME:str="phisingData.csv"
+
+
+## Inside ingested folder
+TRAIN_DATA_FILE="train.csv"
+TEST_DATA_FILE="test.csv"
+
+
+DATA_INGESTION_COLLECTION_NAME:str="NetworkData"
+DATA_INGESTION_DATABASE_NAME:str="SUMITAV"
+DATA_INGESTION_DIR_NAME:str="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"  ## Raw Data
+DATA_INGESTION_INGESTED_STORE_DIR:str="ingested" ## Train and Test Data
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
